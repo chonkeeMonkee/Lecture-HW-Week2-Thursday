b1523c203b20f1606bef6be675fadc61e2e6349f
Alexander Montano-Leon <157240345+chonkeeMonkee@users.noreply.github.com>
2026-04-14 01:18:39 +0000

    Endiannes Converter

--- endianness_converter.sv
+++ endianness_converter.sv
@@ -1,1 +1,21 @@
 // Put your code below
+module endianness_converter(
+    input clk,
+    input [31:0] word, 
+    input endianness_in, 
+    input endianness_out, 
+    output reg [31:0] converted_word
+    );
+
+    always @ (posedge clk) 
+        begin 
+            if(endianness_in == endianness_out) begin
+                converted_word <= word;
+            end else begin
+                //Conversion
+                converted_word <= {word[7:0], word[15:8], word[23:16], word[31:24]};
+            end
+                
+        end 
+
+    endmodule
